ace51e24e9ef73dec8a2c1dc2206042fdb9cf37d
floopv <felobaternaeem3@gmail.com>
2025-12-01 19:27:27 +0200

    Add files via upload

---
+++ CinemaReservationSystem/Areas/Customer/Controllers/CheckoutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaReservationSystem.Areas.Customer.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult success()
+        {
+            return View();
+        }
+        public IActionResult cancel()
+        {
+            return View();
+        }
+    }
+}
